d7d53e522b52c23d33b5c0530ecd971472a73e94
Mark Ha <mha@appnexus.com>
2013-10-29 16:37:57 -0400

    prevent mraid popups

--- sdk/ANAdWebViewController.m
+++ sdk/ANAdWebViewController.m
@@ -112,6 +112,8 @@ typedef enum _ANMRAIDState
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
+    // prevent pop-ups from showing
+    [webView stringByEvaluatingJavaScriptFromString:@"window.alert=function(){};"];
 	if (self.completedFirstLoad)
 	{
 		NSURL *URL = [request URL];
