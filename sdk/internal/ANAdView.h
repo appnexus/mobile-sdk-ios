01874172327ac10e5851de030156e1a6e5167b68
Mark Ha <mha@appnexus.com>
2013-11-22 16:49:09 -0500

    MS-356 another typo

--- sdk/internal/ANAdView.h
+++ sdk/internal/ANAdView.h
@@ -31,7 +31,7 @@
     UIButton *__closeButton;
 }
 
-#pragma mark Depecrated Properties
+#pragma mark Deprecrated Properties
 
 // This property is deprecated, use "opensInNativeBrowser" instead
 @property (nonatomic, readwrite, assign) BOOL clickShouldOpenInBrowser DEPRECATED_ATTRIBUTE;
