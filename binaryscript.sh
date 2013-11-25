bf8072350de510f9e54269e454d1b4ac5ad5ecfc
Mark Ha <mha@appnexus.com>
2013-11-25 17:59:49 -0500

    MS-358 resources need to be passed

--- binaryscript.sh
+++ binaryscript.sh
@@ -2,13 +2,9 @@ rm -r out
 
 xcodebuild -project 'AppNexusSDK.xcodeproj/' -scheme 'AppNexusSDK' -configuration 'Release' -sdk iphoneos7.0 CONFIGURATION_BUILD_DIR='out'
 
-mv out/include/AppNexusSDK out/AppNexusSDK/
-
 mv out/libAppNexusSDK.a out/AppNexusSDK/libAppNexusSDK.a
 
 xcodebuild -project 'ANSDK.xcodeproj/' -scheme 'ANSDK' -configuration 'Release' -sdk iphoneos7.0 CONFIGURATION_BUILD_DIR='out'
-
-mv out/include/ANSDK out/ANSDK/
 
 mv out/libANSDK.a out/ANSDK/libANSDK.a
 
