c9b413754b9a384072ade47856ff9ea79f97016a
Mark Ha <mha@appnexus.com>
2013-11-26 14:17:40 -0500

    MS-358 rename projects to ANSDK and ANSDKFull

--- binaryscript.sh
+++ binaryscript.sh
@@ -1,13 +1,12 @@
 rm -r out
 
-xcodebuild -project 'AppNexusSDK.xcodeproj/' -scheme 'AppNexusSDK' -configuration 'Release' -sdk iphoneos7.0 CONFIGURATION_BUILD_DIR='out'
-
-mv out/libAppNexusSDK.a out/AppNexusSDK/libAppNexusSDK.a
-
 xcodebuild -project 'ANSDK.xcodeproj/' -scheme 'ANSDK' -configuration 'Release' -sdk iphoneos7.0 CONFIGURATION_BUILD_DIR='out'
 
 mv out/libANSDK.a out/ANSDK/libANSDK.a
+mv out/ANSDKResources.bundle out/ANSDK/ANSDKResources.bundle
+
+xcodebuild -project 'ANSDKFull.xcodeproj/' -scheme 'ANSDKFull' -configuration 'Release' -sdk iphoneos7.0 CONFIGURATION_BUILD_DIR='out'
+
+mv out/libANSDKFull.a out/ANSDKFull/libANSDKFull.a
 
 rm -r out/Intermediates
-
-
