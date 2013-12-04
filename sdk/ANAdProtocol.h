2a82493a7ff5236087ab1fd7e324331c5a910e14
Rich Loveland <rloveland@appnexus.com>
2013-12-04 16:22:00 -0500

    Start ad protocol docs.

--- sdk/ANAdProtocol.h
+++ sdk/ANAdProtocol.h
@@ -35,18 +35,50 @@ typedef enum _ANGender
 // types (It's used in the implementation of both banners and
 // interstitials by the SDK).  If you wanted to, you could implement
 // your own ad type using this protocol.
+
 @protocol ANAdProtocol <NSObject>
 
 @required
+// An AppNexus placement ID.  A placement ID is a numeric ID that's
+// associated with a place where ads can be shown.  In our
+// implementations of banner and interstitial ad views, we associate
+// each ad view with a placement ID.
 @property (nonatomic, readwrite, strong) NSString *placementId;
+
+// Represents the width and height of the ad view.  This should match
+// the dimensions associated with the placement ID you're using.
 @property (nonatomic, readwrite, assign) CGSize adSize;
+
+// Determines whether the ad, when clicked, will open the device's
+// native browser.
 @property (nonatomic, readwrite, assign) BOOL opensInNativeBrowser;
+
+// Each ad view is associated with an ad fetcher that does the work of
+// actually requesting ads from the server.
 @property (nonatomic, readwrite, strong) ANAdFetcher *adFetcher;
+
+// Whether the ad view should display PSAs if there are no ads
+// available from the server.
 @property (nonatomic, readwrite, assign) BOOL shouldServePublicServiceAnnouncements;
+
+// The user's location.  See ANLocation.h in this directory for
+// details.
 @property (nonatomic, readwrite, strong) ANLocation *location;
+
+// The reserve price is the minimum bid amount you'll accept to show
+// an ad.  Use this with caution, as it can drastically reduce fill
+// rates (i.e., you will make less money).
 @property (nonatomic, readwrite, assign) CGFloat reserve;
+
+// The user's age.  This can contain a numeric age, a birth year, or a
+// hyphenated age range.  For example, "56", "1974", or "25-35".
 @property (nonatomic, readwrite, strong) NSString *age;
+
+// The user's gender.  See the _ANGENDER struct above for details.
 @property (nonatomic, readwrite, assign) ANGender gender;
+
+// Used to pass custom keywords across different mobile ad server and
+// SDK integrations.
 @property (nonatomic, readwrite, strong) NSMutableDictionary *customKeywords;
 
 - (NSString *)adType;
