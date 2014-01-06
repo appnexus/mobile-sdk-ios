40f5fefbe7531604e82b53a319f739ef3e1d7c7b
Mark Ha <mha@appnexus.com>
2014-01-06 13:18:42 -0500

    MS-425 sizeChange event not being fired
    call setCurrentSize first so that sizeChange event is called
    on intial load, fire mraid events last
    mraid.js don't fire viewableChange events if state is loading
    print refresh time
    other cleanup

--- sdk/Resources/MRAID.bundle/mraid.js
+++ sdk/Resources/MRAID.bundle/mraid.js
@@ -356,6 +356,7 @@
  };
  
  mraid.util.viewableChangeEvent=function(is_viewable_now){
+ if(state==='loading') return;
  is_viewable = is_viewable_now;
  for(var i=0;i<listeners['viewableChange'].length;i++){
  listeners['viewableChange'][i](is_viewable_now);
