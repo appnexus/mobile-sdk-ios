8b72acb0ada1b7a2b99f3a69be26fdc60660d505
Jose Cabal-Ugaz <josecu@appnexus.com>
2014-02-05 10:58:16 -0500

    Added Apache license to new MRAID test files

--- tests/TestClasses/TestUtils/ANMRAIDTestResponses.m
+++ tests/TestClasses/TestUtils/ANMRAIDTestResponses.m
@@ -1,10 +1,17 @@
-//
-//  ANMRAIDTestResponses.m
-//  Tests
-//
-//  Created by Jose Cabal-Ugaz on 1/27/14.
-//
-//
+/*   Copyright 2014 APPNEXUS INC
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "ANMRAIDTestResponses.h"
 
@@ -13,7 +20,5 @@
 + (NSString *)basicMRAIDBannerWithSelectorName:(NSString *)selector {
     return [self createAdsResponse:@"banner" withWidth:320 withHeight:50 withContent:[NSString stringWithFormat:@"<script type=\\\"text/javascript\\\" src=\\\"mraid.js\\\"></script><script type=\\\"text/javascript\\\">document.write('<div style=\\\"background-color:#EF8200;height:1000px;width:1000px;\\\"><p>%@</p></div>');</script>", selector]];
 }
-
-
 
 @end
