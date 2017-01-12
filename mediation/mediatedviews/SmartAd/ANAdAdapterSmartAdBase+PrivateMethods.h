1498f1c4b528bcc439fe785ce71a7afcad30758b
Punnaghai Puviarasu <ppuviarasu@appnexus.com>
2017-01-12 14:11:07 -0500

    modified copyright descriptions

--- mediation/mediatedviews/SmartAd/ANAdAdapterSmartAdBase+PrivateMethods.h
+++ mediation/mediatedviews/SmartAd/ANAdAdapterSmartAdBase+PrivateMethods.h
@@ -1,4 +1,4 @@
-/*   Copyright 2015 APPNEXUS INC
+/*   Copyright 2016 APPNEXUS INC
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-
 
 #import "ANAdAdapterSmartAdBase.h"
 
