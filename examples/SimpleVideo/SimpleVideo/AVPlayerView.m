a09130ac020c25d6058fd927310082931afbd7e3
Punnaghai Puviarasu <ppuviarasu@appnexus.com>
2017-02-13 11:49:50 -0500

    SimpleVideo sample app changes

--- examples/SimpleVideo/SimpleVideo/AVPlayerView.m
+++ examples/SimpleVideo/SimpleVideo/AVPlayerView.m
@@ -1,21 +1,30 @@
-//
-//  AVPlayerView.m
-//  SimpleVideo
-//
-//  Created by Punnaghai Puviarasu on 2/6/17.
-//  Copyright © 2017 AppNexus. All rights reserved.
-//
+/*   Copyright 2017 APPNEXUS INC
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "AVPlayerView.h"
 
+
 @implementation AVPlayerView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
++ (Class)layerClass {
+    return [AVPlayerLayer class];
 }
-*/
-
+- (AVPlayer*)player {
+    return [(AVPlayerLayer *)[self layer] player];
+}
+- (void)setPlayer:(AVPlayer *)player {
+    [(AVPlayerLayer *)[self layer] setPlayer:player];
+}
 @end
