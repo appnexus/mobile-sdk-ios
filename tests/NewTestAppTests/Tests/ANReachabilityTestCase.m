d6f576e7fc9fc69a59e1b8854fa5e024053fe863
David Reeder <dreeder@appnexus.com>
2017-02-23 18:15:51 -0500

    WHAT   Fix per https://github.com/appnexus/mobile-sdk-ios/pull/12 .
    REPRO STEPS
      This is a build analysis error, so the test environment is the build results within Xcode.
    
      To test in Xcode run Product -> Analyze.
    
      Before the fix notice that there are analysis errors in class ANReachability,
        in methods reachabilityWithHostName: and reachabilityWithAddress:.
      After the fix notice that there are NO analysis errors.
    
    DESCRIPTION
      Reorganizes the use of allocated objects such that they are always
        allocated in the scope in which they are returned.
    
    UNIT TESTS
      tests/NewTestAppTests/Tests/ANReachabilityTestCase.m

---
+++ tests/NewTestAppTests/Tests/ANReachabilityTestCase.m
@@ -0,0 +1,68 @@
+//
+//  ANReachabilityTestCase.m
+//  NewTestApp
+//
+//  Created by David Reeder on 23/02/2017.
+//  Copyright © 2017 AppNexus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import <arpa/inet.h>
+
+#import "ANReachability.h"
+
+
+
+
+@interface ANReachabilityTestCase : XCTestCase
+    //EMPTY
+@end
+
+
+
+@implementation ANReachabilityTestCase
+
+- (void)setUp     { [super setUp]; }
+- (void)tearDown  { [super tearDown]; }
+
+
+- (void)testHostname
+{
+    NSString  *hostNameString  = @"appnexus.com";
+    //NSString  *localhostName  = @"localhost";
+
+    //
+    ANReachability  *hostname  = [ANReachability reachabilityWithHostName:hostNameString];
+    XCTAssertNotNil(hostname);
+
+    XCTAssertTrue([hostname currentReachabilityStatus]);
+}
+
+
+- (void) testAppNexusIPAddress
+{
+    NSString           *hostNumberString  = @"68.67.154.120";   // % dig appnexus.com
+    struct in_addr      hostNumber;
+    struct sockaddr_in  socketAddress;
+
+    int  rval  = -1;
+
+
+    //
+    rval = inet_aton([hostNumberString cStringUsingEncoding:NSASCIIStringEncoding], &hostNumber);
+    XCTAssertEqual(rval, 1);
+
+    bzero(&socketAddress, sizeof(socketAddress));
+    socketAddress.sin_len           = sizeof(socketAddress);
+    socketAddress.sin_family        = AF_INET;
+    socketAddress.sin_addr.s_addr   = htonl(hostNumber.s_addr);
+
+    ANReachability  *appNexusIPAddress  = [ANReachability reachabilityWithAddress:&socketAddress];
+    XCTAssertNotNil(appNexusIPAddress);
+
+    XCTAssertTrue([appNexusIPAddress currentReachabilityStatus]);
+}
+
+
+@end
