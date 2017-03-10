96df0dc51b98d6b7a8a6ec3722a25c35ca26c632
Punnaghai Puviarasu <ppuviarasu@appnexus.com>
2017-03-10 13:39:17 -0500

    removed calabash framework inclusion

--- examples/SimpleVideo/calabash.framework/Versions/A/Headers/LPWebViewUtils.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface LPWebViewUtils : NSObject
-
-+ (BOOL) isWebView:(id) object;
-+ (BOOL) isIFrameResult:(NSDictionary *)result;
-
-@end
--- examples/SimpleVideo/calabash.framework/Versions/A/Headers/UIWebView+LPWebView.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "LPWebViewProtocol.h"
-
-@interface UIWebView (UIWebView_LPWebView) <LPWebViewProtocol>
-
-@end
