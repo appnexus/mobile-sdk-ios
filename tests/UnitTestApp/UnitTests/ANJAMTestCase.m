d98473854a9a87404a58eae6aad0860f9016b46c
Punnaghai Puviarasu <ppuviarasu@appnexus.com>
2019-09-10 15:15:55 -0400

    formatting test

--- tests/UnitTestApp/UnitTests/ANJAMTestCase.m
+++ tests/UnitTestApp/UnitTests/ANJAMTestCase.m
@@ -1,11 +1,11 @@
 /*   Copyright 2019 APPNEXUS INC
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -150,10 +150,10 @@
 
 - (void)stubRequestWithResponse:(NSString *)responseName {
     NSBundle *currentBundle = [NSBundle bundleForClass:[self class]];
-    NSString *baseResponse = [NSString stringWithContentsOfFile: [currentBundle pathForResource:responseName
-                                                                                         ofType:@"json" ]
-                                                       encoding: NSUTF8StringEncoding
-                                                          error: nil ];
+    NSString *baseResponse = [NSString stringWithContentsOfFile:
+                                [currentBundle pathForResource:responseName ofType:@"json" ]
+                                encoding: NSUTF8StringEncoding
+                                error: nil ];
 
     ANURLConnectionStub  *requestStub  = [[ANURLConnectionStub alloc] init];
 
