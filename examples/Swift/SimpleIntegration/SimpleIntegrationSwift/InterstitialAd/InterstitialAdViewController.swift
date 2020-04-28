8e1bb66db021619de76a66bad10b577c52b1e209
Abhishek Sharma <asharma@appnexus.com>
2020-04-28 14:58:05 +0000

    SwiftSampleApp
    Squashed commit of the following:
    
    commit dc05fbd876c814e6ac13ae95455ad45ea7d86fa5
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Mon Apr 27 20:02:45 2020 +0530
    
        Updated New API
    
    commit 0c4f801242df487dbd6ee3dce0a36eb126514396
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Mon Apr 27 19:37:47 2020 +0530
    
        Updated Project Name
    
    commit 8cf41930d0d84aa6f58b0c3e91482fb43f4bd4ff
    Merge: f95de0d 34a1a17
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Sat Apr 25 00:56:40 2020 +0530
    
        Merge remote-tracking branch 'origin/develop' into SingleSampleAppSwift
    
    commit f95de0de57a9807cf9236e954abff88ef2239468
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Mon Mar 2 21:12:15 2020 +0530
    
        Removed Ad
    
    commit 6071922fd35fc58bcc7879795dc8e1f04fe655a8
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Mon Mar 2 20:44:14 2020 +0530
    
        App Name : SimpleIntegration
    
    commit d0450fa7e92f6b1634b7b9f083e5cbae286c1319
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Mon Mar 2 14:19:04 2020 +0530
    
        Updated Storyboard name
    
    commit 8377fb40be86c1c1df8d08649e3176517e84c8f4
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Thu Feb 27 14:44:46 2020 +0530
    
        Reverted SDk Change
    
    commit 6f607cfdef1416463a9ee237c3eafa9aaad1d02e
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Thu Feb 27 14:43:54 2020 +0530
    
        Removed SceneDelegate
    
    commit cedfe16f8bdab2c19b522a475825d81db80cf7df
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Wed Feb 26 15:52:40 2020 +0530
    
        shouldServePublicServiceAnnouncements removed
    
    commit a414a6e07acc0a1a5dd210bab189bff3cba205ec
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Wed Feb 26 15:49:19 2020 +0530
    
        Removed ANClickThroughAction
    
    commit cadb65f3ccce5902a56ff65cda88e101a6a09d0e
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Wed Feb 26 15:45:19 2020 +0530
    
        Added Comments
    
    commit 5f33c175c752e417995e65455c4b4c4ea53640b3
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Wed Feb 26 15:37:32 2020 +0530
    
        Updated Comments
    
    commit 9a564b12c8db7f77798075788656d740d06c1980
    Author: Abhishek Sharma <asharma@appnexus.com>
    Date:   Wed Feb 26 14:53:19 2020 +0530
    
        Updated Sample App

---
+++ examples/Swift/SimpleIntegration/SimpleIntegrationSwift/InterstitialAd/InterstitialAdViewController.swift
@@ -0,0 +1,44 @@
+/*   Copyright 2020 APPNEXUS INC
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import UIKit
+import AppNexusSDK
+
+class InterstitialAdViewController: UIViewController , ANInterstitialAdDelegate {
+    
+    var interstitialAd: ANInterstitialAd?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Interstitial Ad"
+        
+        interstitialAd = ANInterstitialAd(placementId: "1281482")
+        interstitialAd!.delegate = self
+        interstitialAd!.clickThroughAction = ANClickThroughAction.openSDKBrowser
+        interstitialAd!.load()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - ANInterstitialAdDelegate
+    func adDidReceiveAd(_ ad: Any) {
+        print("adDidReceiveAd")
+        interstitialAd!.display(from: self)
+    }
+    
+    func ad(_ ad: Any, requestFailedWithError error: Error) {
+        print("Ad request Failed With Error")
+    }
+}
+
