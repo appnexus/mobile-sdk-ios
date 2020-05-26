d17e4cb1be45d5f183038f7c277424ac214e0f3a
Punnaghai Puviarasu <ppuviarasu@appnexus.com>
2020-05-26 16:19:19 -0400

    added the if logic and only removed the else part

--- sdk/sourcefiles/internal/ANAdFetcherBase.m
+++ sdk/sourcefiles/internal/ANAdFetcherBase.m
@@ -53,7 +53,9 @@
 {
     self = [super init];
     if (!self)  { return nil; }
-
+    //
+    [self setup];
+    
     return  self;
 }
 
@@ -76,6 +78,13 @@
     self.fetcherMARManager = marManager;
 
     return  self;
+}
+
+- (void)setup
+{
+    if([ANGDPRSettings canAccessDeviceData]){
+        [NSHTTPCookieStorage sharedHTTPCookieStorage].cookieAcceptPolicy = NSHTTPCookieAcceptPolicyAlways;
+    } 
 }
 
 -(void) stopAdLoad{
