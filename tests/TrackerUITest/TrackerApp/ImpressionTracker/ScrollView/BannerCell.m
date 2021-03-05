2e768ad311861ad98a2341db05e69f8e2a60eb65
mobileSDK <ppuviarasu@appnexus.com>
2021-03-05 10:57:01 -0500

    adding banner cells

---
+++ tests/TrackerUITest/TrackerApp/ImpressionTracker/ScrollView/BannerCell.m
@@ -0,0 +1,23 @@
+//
+//  BannerCell.m
+//  BannerLoadPerformance
+//
+//  Created by Punnaghai Puviarasu on 11/3/20.
+//
+
+#import "BannerCell.h"
+
+@implementation BannerCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ tests/TrackerUITest/TrackerApp/ImpressionTracker/ScrollView/ContentCell.h
@@ -0,0 +1,16 @@
+//
+//  ContentCell.h
+//  BannerLoadPerformance
+//
+//  Created by Punnaghai Puviarasu on 11/3/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContentCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@end
+
+NS_ASSUME_NONNULL_END
