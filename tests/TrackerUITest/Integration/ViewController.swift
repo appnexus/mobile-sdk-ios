0f8aac8f768a2453485a2e3d7831acf0dbcb6921
abhisheksharma <abhisheksharma@vpn-10-249-108-18.sin3.appnexus.com>
2021-07-15 01:25:04 +0530

    Merged unit and tracker app

---
+++ tests/TrackerUITest/Integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Integration
+//
+//  Created by abhisheksharma on 14/07/21.
+//  Copyright © 2021 Xandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tests/TrackerUITest/IntegrationTests/IntegrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  IntegrationTests.swift
+//  IntegrationTests
+//
+//  Created by abhisheksharma on 14/07/21.
+//  Copyright © 2021 Xandr. All rights reserved.
+//
+
+import XCTest
+@testable import Integration
+
+class IntegrationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
