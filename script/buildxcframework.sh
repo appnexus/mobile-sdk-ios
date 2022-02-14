9d0032c2db917097ff18c7801706fa3921ca81fa
Abhishek Sharma <asharma@appnexus.com>
2022-02-14 12:19:07 +0000

    Script to build XCFramework and zip
    Squashed commit of the following:
    
    commit 4a66e631fabb74e996f0f4776c5cb6052a255753
    Author: abhisheksharma <abhisheksharma@vpn-10-249-108-18.sin3.appnexus.com>
    Date:   Wed Feb 2 02:35:45 2022 +0530
    
        Updated inline comment
    
    commit 2130648e5036a2d8daa7f26a3bbfa7ab7a5b2581
    Author: abhisheksharma <abhisheksharma@vpn-10-249-108-18.sin3.appnexus.com>
    Date:   Wed Feb 2 02:34:11 2022 +0530
    
        Updated build script
    
    commit a684dce76493a429f1a0cd8cafa9757869ba0e7c
    Author: abhisheksharma <abhisheksharma@vpn-10-249-108-18.sin3.appnexus.com>
    Date:   Tue Feb 1 21:04:12 2022 +0530
    
        Added script to buildAppNexusSDK.xcframework.zip

--- script/buildxcframework.sh
+++ script/buildxcframework.sh
@@ -1,34 +1,44 @@
+#!/bin/bash
+# buildxcframework.sh
+#	The AppNexusSDK.xcframework and AppNexusNativeSDK.xcframework are built using the script file.
+# 	These instructions will create a zip file named AppNexusSDK.xcframework.zip that contains three frameworks: AppNexusSDK.xcframework, OMSDK_Appnexus.xcframework, and AppNexusNativeSDK.xcframework.
+# 	The AppNexusSDK.xcframework with OMSDK_Appnexus.xcframework or AppNexusNativeSDK.xcframework framework with OMSDK_Appnexus can be used in any combination.
+# 	iphoneos archive, or iphonesimulator archive will be used to generate the xcframework.
+# 	All temporary binaries will be removed using rm commands once the xcframework has been produced and the zip has been created.
+
+
 # Output directory name
-OUTDIR=$1+"Framework"
-# Scheme name AppNexusSDK OR AppNexusNativeSDK
-SCHEMENAME=$1
-# for iPhones
+#Start Building AppNexusSDK.xcframework
+
+# Schema name AppNexusSDK OR AppNexusNativeSDK
+SCHEMANAME="AppNexusSDK"
+# Schema name for iPhones
 OD_DEVICE="iphoneos"
-# for simulator
+# Schema name for simulator
 OD_SIMULATOR="iphonesimulator"
 
 
 # Build archive for iphonesimulator
 xcodebuild archive \
- -scheme "$SCHEMENAME" \
- -archivePath ./"$OD_SIMULATOR"/"$SCHEMENAME"-"$OD_SIMULATOR".xcarchive \
+ -scheme "$SCHEMANAME" \
+ -archivePath ./"$OD_SIMULATOR"/"$SCHEMANAME"-"$OD_SIMULATOR".xcarchive \
  -sdk "$OD_SIMULATOR" \
  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
  SKIP_INSTALL=NO
 
 # Build archive for iphoneos
 xcodebuild archive \
- -scheme "$SCHEMENAME" \
- -archivePath ./"$OD_DEVICE"/"$SCHEMENAME"-"$OD_DEVICE".xcarchive \
+ -scheme "$SCHEMANAME" \
+ -archivePath ./"$OD_DEVICE"/"$SCHEMANAME"-"$OD_DEVICE".xcarchive \
  -sdk "$OD_DEVICE" \
  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
  SKIP_INSTALL=NO
  
  # Build XCFramework for using iphonesimulator and iphoneos archive
  xcodebuild -create-xcframework \
-    -framework ./"$OD_SIMULATOR"/"$SCHEMENAME"-"$OD_SIMULATOR".xcarchive/Products/Library/Frameworks/"$SCHEMENAME".framework \
-     -framework ./"$OD_DEVICE"/"$SCHEMENAME"-"$OD_DEVICE".xcarchive/Products/Library/Frameworks/"$SCHEMENAME".framework \
-   -output ./../XCFramework/"$SCHEMENAME".xcframework
+    -framework ./"$OD_SIMULATOR"/"$SCHEMANAME"-"$OD_SIMULATOR".xcarchive/Products/Library/Frameworks/"$SCHEMANAME".framework \
+     -framework ./"$OD_DEVICE"/"$SCHEMANAME"-"$OD_DEVICE".xcarchive/Products/Library/Frameworks/"$SCHEMANAME".framework \
+   -output ./../XCFramework/"$SCHEMANAME".xcframework
 
 # Zip XCFramework
 #zip -r "$OUTDIR" ../XCFramework
@@ -37,3 +47,54 @@ xcodebuild archive \
 rm -rf ./iphonesimulator
 rm -rf ./iphoneos
 #rm -rf ./../XCFramework
+
+
+SCHEMENAME_NATIVESDK="AppNexusNativeSDK"
+
+
+
+# Build archive for iphonesimulator
+xcodebuild archive \
+ -scheme "$SCHEMANAME_NATIVESDK" \
+ -archivePath ./"$OD_SIMULATOR"/"$SCHEMANAME_NATIVESDK"-"$OD_SIMULATOR".xcarchive \
+ -sdk "$OD_SIMULATOR" \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+ SKIP_INSTALL=NO
+
+
+
+ # Build archive for iphoneos
+xcodebuild archive \
+ -scheme "$SCHEMANAME_NATIVESDK" \
+ -archivePath ./"$OD_DEVICE"/"$SCHEMANAME_NATIVESDK"-"$OD_DEVICE".xcarchive \
+ -sdk "$OD_DEVICE" \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+ SKIP_INSTALL=NO
+ 
+ # Build XCFramework for using iphonesimulator and iphoneos archive
+ xcodebuild -create-xcframework \
+    -framework ./"$OD_SIMULATOR"/"$SCHEMANAME_NATIVESDK"-"$OD_SIMULATOR".xcarchive/Products/Library/Frameworks/"$SCHEMANAME_NATIVESDK".framework \
+     -framework ./"$OD_DEVICE"/"$SCHEMANAME_NATIVESDK"-"$OD_DEVICE".xcarchive/Products/Library/Frameworks/"$SCHEMANAME_NATIVESDK".framework \
+   -output ./../XCFramework/"$SCHEMANAME_NATIVESDK".xcframework
+
+# Removed archive and output folder
+rm -rf ./iphonesimulator
+rm -rf ./iphoneos
+
+ 
+# Copy OMSDK_Appnexus.xcframework from Viewability to XCFramework
+cp -a "./sourcefiles/Viewability/OMSDK_Appnexus.xcframework" "../XCFramework" 
+
+
+# Move to XCFramework folder
+cd ..
+cd XCFramework
+
+# create zip with name AppNexusSDK.xcframework.zip which included   AppNexusSDK.xcframework OMSDK_Appnexus.xcframework and AppNexusNativeSDK.xcframework
+zip -r ../AppNexusSDK.xcframework.zip *
+cd ..
+
+# Removed XCFramework  folder
+
+rm -rf XCFramework
+
